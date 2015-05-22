30bef9101fa2db6c6d5920e15bb7aed199f27559
ablashaw <andrew.blashaw@gmail.com>
2015-05-21 19:17:33 -0700

    week 3 hw cacheMatrix commit with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix and cacheSolve are functions designed to compute the inverse
+# of an input matrix and/or quickly return the inverse matrix, avoiding
+# unncessary computations
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # creates a variable 'x' containing a list whose elements are functions that
+    # define a matrix, return the input matrix values, calculate its inverse,
+    # and retrive the inverse matrix from "cached" memory
+    
+    m <- NULL  # initiate variable m to store result from matrix solve()
+    set <- function(y) {
+        x <<- y  # assign initial matrix values
+        m <<- NULL  # reset inverse matrix for new input matrix 'x'
+    }
+    get <- function() {
+        x  # return input matrix 'x'
+    }
+    setsolve <- function(xsolve) {
+        m <<- xsolve  # store inverse matrix result as 'm'
+    }
+    getsolve <- function() {
+        m  # return inverse matrix result, if available, or NULL
+    }
+    
+    # return a list with functions as elements of the list
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # determines if the inverse of matrix 'x' is in memory and calculates it if
+    # unknown, then returns the matrix inverse of 'x'
+    
+    m <- x$getsolve()  # retrieving inverse matrix 'm'
+    
+    if (!is.null(m)) {  # checking if inverse has already been calculated
+        message("getting cached data")  # inverse is already in memory
+        return(m)  # returning cached result for inverse
+    }
+    
+    data <- x$get()  # retriving matrix 'x'
+    m <- solve(data, ...)  # calculating inverse for matrix 'x'
+    x$setsolve(m)  # caching inverse matrix
+    m
 }
